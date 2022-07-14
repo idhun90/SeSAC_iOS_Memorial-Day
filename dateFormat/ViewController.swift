c020063d3a5449139c130e53f264ef5ed6a0f60b
idhun90 <idhun90@naver.com>
2022-07-14 17:54:53 +0900

    Initial Commit

---
+++ dateFormat/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  dateFormat
+//
+//  Created by 이도헌 on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        // 수업 공부 내용
+//        let format = DateFormatter()
+//        format.dateFormat = "M월 d일, yy년"
+//        //format.locale
+//
+//        let result = format.string(from: Date())
+//        print(result, Date())
+//
+//        let word = "3월 2일, 19년"
+//        let dateResult = format.date(from: word)
+//        print(dateResult)
+//
+//
+//        let now = Date()
+//        print("현재 시간은 \(now)입니다.")
+        
+    }
+
+
+}
+
