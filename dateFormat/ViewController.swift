ef2e4e112eb5a5e2ae586ee3f45e427adefadc26
idhun90 <idhun90@naver.com>
2022-07-14 19:18:27 +0900

    DatePicker를 클릭하면 클릭한 날짜가 출력되게끔 작성 완료. 추가 작업 필요. 첫 로딩 때 보여줄 레이블 값 그리고 100/200/300//400일 날짜 표시 구현 필요.

--- dateFormat/ViewController.swift
+++ dateFormat/ViewController.swift
@@ -5,16 +5,7 @@
 //  Created by 이도헌 on 2022/07/14.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
-        // 수업 공부 내용
+// 수업 공부 내용
 //        let format = DateFormatter()
 //        format.dateFormat = "M월 d일, yy년"
 //        //format.locale
@@ -29,9 +20,79 @@ class ViewController: UIViewController {
 //
 //        let now = Date()
 //        print("현재 시간은 \(now)입니다.")
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var day100Label: UILabel!
+    @IBOutlet weak var day200Label: UILabel!
+    @IBOutlet weak var day300Label: UILabel!
+    @IBOutlet weak var day400Label: UILabel!
+    
+    @IBOutlet weak var imageView100: UIImageView!
+    @IBOutlet weak var imageView200: UIImageView!
+    @IBOutlet weak var imageView300: UIImageView!
+    @IBOutlet weak var imageView400: UIImageView!
+    
+    @IBOutlet var titleLabelList: [UILabel]!
+    @IBOutlet var resultLableList: [UILabel]!
+    @IBOutlet var imageViewList: [UIImageView]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        
+        // D+100/200/300/400 레이블 디자인
+        for i in 0...titleLabelList.count-1 {
+            let labelTitle = ["D+100", "D+200", "D+300", "D+400"]
+            titleLabelList[i].text = labelTitle[i]
+            titleLabelList[i].textColor = .white
+            titleLabelList[i].font = .boldSystemFont(ofSize: 25)
+        }
+        
+        // 디데이 결과 레이블 디자인
+        for j in resultLableList {
+            j.textColor = .white
+            j.textAlignment = .center
+        }
+        
+        // 이미지뷰 디자인
+        for i in imageViewList {
+            i.backgroundColor = .systemBlue
+            i.layer.masksToBounds = true
+            i.layer.cornerRadius = 10
+        }
+        
+        
         
     }
 
-
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+//        dateFormatter.dateStyle = .long // 연도, 월&일 두 개의 레이블로 나눠야 하는지 고민했지만 작업 편의를 위해 일단 보류.
+//        dateFormatter.timeStyle = .none
+        
+        dateFormatter.dateFormat = "YYYY년\nMM월 dd일"
+        
+        let date = sender.date // DatePicker sender date를 받아옴
+        
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        print(dateFormatter.string(from: date))
+        
+        let result = dateFormatter.string(from: date)
+        
+        for i in resultLableList {
+            i.text = result
+        }
+        
+    }
+    
 }
 
